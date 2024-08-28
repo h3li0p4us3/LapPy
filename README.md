e1bd3a16b07b9edd38b5b1be5fd809cd85e0e82b
= <h3li0p4us3@proton.me>
2024-08-28 20:33:57 +0330

    add source code, and README.md

---
+++ README.md
@@ -0,0 +1,43 @@
+# LapPy
+
+LapPy is a simple web application built with Python and Flask. It provides a user-friendly interface for sending text to the clipboard and uploading files.
+
+## Installation
+
+To run LapPy, you need to install Flask:
+
+```bash
+pip3 install flask
+```
+
+## Usage
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/h3li0p4us3/LapPy
+```
+
+2. Activate the virtual environment:
+
+```bash
+source venv/bin/activate
+```
+
+3. Run the main script:
+
+```bash
+python src/main.py
+```
+
+4. Copy the local network IP address displayed in the terminal.
+5. Open a web browser and navigate to `http://<IP_ADDRESS>:8080` to access the application.
+
+## Features
+
+- Send text to the clipboard
+- Upload files to the server
+
+## Contributing
+
+Contributions are welcome! Please feel free to fork and modify the code as needed.
---
+++ src/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, send_file, render_template
+import subprocess
+import os
+
+app = Flask(__name__, template_folder='front')
+
+os.makedirs('uploads', exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/send-text', methods=['POST'])
+def send_text():
+    data = request.form
+    text = data.get('text', '')
+    savetoClipboard(text)
+    return jsonify({'message': 'Text sent successfully'}), 200
+
+
+
+def savetoClipboard(text):
+    command = ["wl-copy", " "] # use wl-copy command for adding the text to clipboard. (should be different on other systems)
+    try:
+        subprocess.run(command + [text], check=True)
+        print(f"Text '{text}' saved to clipboard")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to save text to clipboard: {e}")
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    if file:
+        try:
+            filename = os.path.basename(file.filename)
+            # save file in uploads/ folder
+            file.save(os.path.join('uploads', filename))
+            return f"File '{filename}' uploaded successfully"
+        except IOError as e:
+            return f"Error uploading file: {str(e)}"
+        except Exception as e:
+            return f"An unexpected error occurred: {str(e)}"
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080) # if you set to 80, you will need root permission.
+
+# defining host as 0.0.0.0 is required for running the program on all addresses.
